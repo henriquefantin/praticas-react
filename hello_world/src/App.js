a7a1eed1a4bbeebd77251d457fac700d7891db85
henriquefantin <henrique.fantin.ns@gmail.com>
2026-04-12 17:09:48 -0300

    Adicionar projeto hello_world

---
+++ hello_world/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="centralizar text-white">
+      <header className="cabecalho">
+        <h3>Hello World Title</h3>
+        <div className='p-1'>
+          <p className='justificado ml-5'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ad eveniet quae aperiam iure sint earum, maxime facilis praesentium. Libero possimus enim ipsum perferendis beatae, iure tempore tempora recusandae soluta!
+          </p>
+          <p className='justificado ml-5 mt-3'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nulla temporibus, vitae voluptates ea sint fugiat facere. Odit eius perferendis porro, cum ad atque quam tenetur totam repellat neque. Eos?
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+ 
