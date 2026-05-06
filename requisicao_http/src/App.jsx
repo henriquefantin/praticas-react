d14ddfedc3a10fdb1ce449f5e46e124f5a9251c9
Henrique Fantin <henrique.fantin.ns@gmail.com>
2026-05-06 20:54:00 -0300

    Atualização com hook useFetch e exercicio de requisição DELETE

--- requisicao_http/src/App.jsx
+++ requisicao_http/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
+
+// Importando o hook personalizado
+import { useFetch } from './hooks/useFetch';
 
 const apiUrl = 'http://localhost:3000/products';
 
@@ -9,15 +12,18 @@ function App() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
+  // Recebe os dados do hook personalizado
+  const { data: items, httpConfig, loading, error } = useFetch(apiUrl);
+
   // Buscar os produtos da API
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch(apiUrl);
+  //     const data = await response.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
 
   // Adicionar produtos
   const handleAddProduct = async (event) => {
@@ -30,35 +36,64 @@ function App() {
     };
 
     // Envia a requisição POST para a API
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(product),
-    });
+    // const response = await fetch(apiUrl, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    // Carrega a resposta da API e atualiza a lista de produtos
-    const addedProduct = await response.json();
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    // // Carrega a resposta da API e atualiza a lista de produtos
+    // const addedProduct = await response.json();
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
+    httpConfig(product, "POST", null);
     setName("");
     setPrice("");
+  }
+
+  // Excluir produtos
+  const handleDeleteProduct = async (id) => {
+    httpConfig(null, "DELETE", id);
   }
 
   return (
     <>
       <div className="App">
         <h2>Lista de produtos</h2>
-        <ul>
-          {
-            products.map((prod) => (
-              <li key={prod.id}>
-                {prod.name} - R$ {prod.price}
-              </li>
-            ))
-          }
-        </ul>
+        {/* Loading */}
+        {loading && <p>Carregando...</p>}
+        {/* Error */}
+        {error && <p>{error}</p>}
+        {
+          !error && (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Preço</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  items && items.map((prod) => (
+                    <tr key={prod.id}>
+                      <td>{prod.id}</td>
+                      <td>{prod.name}</td>
+                      <td>R$ {prod.price}</td>
+                      <td>
+                        <button onClick={() => handleDeleteProduct(prod.id)}>Excluir</button>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          )
+        }
         <div className="add-product">
           <form onSubmit={handleAddProduct}>
             <label>
@@ -79,7 +114,9 @@ function App() {
                 onChange={(e) => setPrice(e.target.value)}
               />
             </label>
-            <button type="submit">Adicionar Produto</button>
+            {/* Remover botão quando estiver carregando */}
+            {loading && <button type="submit" disabled>Aguarde</button>}
+            {!loading && <button type="submit">Adicionar Produto</button>}
           </form>
         </div>
       </div>
