8201371ef6f46b373cc3ec74f09666c8d78f267f
henriquefantin <henrique.fantin.ns@gmail.com>
2026-05-10 21:03:52 -0300

    Atualização Navlink

--- react_router/src/components/Navbar.jsx
+++ react_router/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import './Navbar.css'
 
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">About</Link> */}
+
+      <NavLink
+        to="/"
+        // className={({isActive}) => (isActive ? 'ativo' : 'nao-ativo')}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
   )
 }
