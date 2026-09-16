24cfb7ada3a304e8962bca9c8232b54f99958838
henriquefantin <henrique.fantin.ns@gmail.com>
2026-09-15 21:53:39 -0300

    feat(useCallback): add example useCallback

--- react_hooks/src/pages/Home.jsx
+++ react_hooks/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import HookUseEffect from "../components/HookUseEffect"
 import HookUseReducer from "../components/HookUseReducer"
 import HookUseState from "../components/HookUseState"
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallback from "../components/HookUseCallback";
 
 // useContext
 import { useContext } from "react";
@@ -19,6 +20,7 @@ const Home = () => {
             <p>Context: {contextValue}</p>
             <hr />
             <HookUseRef />
+            <HookUseCallback />
         </div>
     )
 }
