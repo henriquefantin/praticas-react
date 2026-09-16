9353244bbc64f7ca365d01936a899b611cf7b23c
henriquefantin <henrique.fantin.ns@gmail.com>
2026-09-15 21:37:02 -0300

    feat(useRef): add DOM manipulation focus

--- react_hooks/src/components/HookUseRef.jsx
+++ react_hooks/src/components/HookUseRef.jsx
@@ -9,14 +9,30 @@ const HookUseRef = () => {
         numberRef.current = numberRef.current + 1;
     });
 
+    // useRef e DOM
+    const inputRef = useRef();
+    const [text, setText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setText("");
+        inputRef.current.focus();
+    }
+
     return (
         <div>
-            <h2>Use Ref</h2>
+            <h2>UseRef</h2>
             <p>O componente renderizou: {numberRef.current}</p>
             <p>Counter A: {counter}</p>
             <p>Counter B: {counterB}</p>
             <button onClick={() => setCounter(counter+1)}>ContA</button>
             <button onClick={() => setCounterB(counterB+1)}>ContB</button>
+            
+            <h2>UseRef e DOM</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+                <input type="submit" value="Enviar" />
+            </form>
             <hr />
         </div>
     )
