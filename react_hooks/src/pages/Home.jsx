7dd6b5273bd37b3f490c92645cb951417a376e71
Henrique Fantin <henrique.fantin.ns@gmail.com>
2026-09-19 15:55:02 -0300

    feat(useImperativeHandle): add example useImperativeHandle and forwardRef

--- react_hooks/src/pages/Home.jsx
+++ react_hooks/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import HookUseRef from "../components/HookUseRef";
 import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
+import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
 
 // useContext
 import { useContext } from "react";
@@ -25,6 +26,7 @@ const Home = () => {
             <HookUseCallback />
             <HookUseMemo />
             <HookUseLayoutEffect />
+            <HookUseImperativeHandle />
         </div>
     )
 }
